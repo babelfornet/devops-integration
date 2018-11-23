960bda6f35d98932318dc00def4da2bab26d0b34
alberto.ferrazzoli@babelfor.net <alberto.ferrazzoli@babelfor.net>
2018-11-23 10:28:47 +0100

    changed hello message

--- DevOpsIntegration/Program.cs
+++ DevOpsIntegration/Program.cs
@@ -6,7 +6,7 @@ namespace DevOpsIntegration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Babel World!");
         }
     }
 }
